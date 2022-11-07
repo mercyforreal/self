b28d53b9160a1ea64bbe1615705a57078b2e8041
unknown <omilude.omololamercy@gmail.com>
2022-11-07 15:29:06 +0100

    about my self

--- newjava.js
+++ newjava.js
@@ -1,6 +1,6 @@
-document.getElementById("self").innerHTML="MY SELF"
-document.getElementById("name").innerHTML="My Name is omilude Omolola Mercy, Im from Osogbo in Osun State."
-document.getElementById("self").innerHTML=""
-document.getElementById("self").innerHTML=""
-document.getElementById("self").innerHTML=""
-document.getElementById("self").innerHTML=""
+document.getElementById('self').innerHTML="MY SELF";
+document.getElementById('name').innerHTML="My Name is omilude Omolola Mercy, Im from Osogbo in Osun State.";
+document.getElementById('lady').innerHTML="Im a lady and chocolate in complexion,my best food is jollof rice with chicken ";
+document.getElementById('colour').innerHTML="My best colour is white and i love reading novel";
+
+
